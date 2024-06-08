cd9c22fd6f45b12b34579450d67728ecb38fc787
Ali Khattab <aliktb@zohomail.eu>
2024-06-08 20:22:19 +0100

    Create navbar

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     output: "export",
 
     /**
-     * Set base path. This is the slug of your GitHub repository.
+     * Set base path.
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
